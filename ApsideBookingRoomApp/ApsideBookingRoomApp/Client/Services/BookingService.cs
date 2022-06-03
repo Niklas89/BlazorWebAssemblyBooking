93076bd96471e06655fe80fb0f307505f487bb14
Niklas <niklas_edelstam@hotmail.com>
2022-06-03 11:11:00 +0200

    added Services in Client

---
+++ ApsideBookingRoomApp/ApsideBookingRoomApp/Client/Services/BookingService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ApsideBookingRoomApp.Client.Services
+{
+    public class BookingService : IBookingService
+    {
+        private readonly HttpClient _http;
+
+        public BookingService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
+
+        public async Task GetBookings()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Booking>>>("api/Booking");
+            if (response != null && response.Data != null)
+                Bookings = response.Data;
+        }
+    }
+}
---
+++ ApsideBookingRoomApp/ApsideBookingRoomApp/Client/Services/IBookingService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ApsideBookingRoomApp.Client.Services
+{
+    public interface IBookingService
+    {
+        List<Booking> Bookings { get; set; }
+        Task GetBookings();
+    }
+}
